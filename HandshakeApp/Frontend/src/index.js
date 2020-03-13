b1bcce9954300288cd83fb6df28e040f26bfdbd9
sourabhgarg7494 <60305244+sourabhgarg7494@users.noreply.github.com>
2020-03-13 01:06:21 -0700

    Update index.js

--- HandshakeApp/Frontend/src/index.js
+++ HandshakeApp/Frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import registerServiceWorker from './registerServiceWorker';
 import store from './js/store/index';
 import { Provider } from 'react-redux';
 
